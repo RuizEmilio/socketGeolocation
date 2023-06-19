a528e9240177781a2df2078244f234830995d10a
RuizEmilio <eruizb2000@alumno.ipn.mx>
2023-06-19 14:22:51 -0600

    socketGeolocation

---
+++ server.js
@@ -0,0 +1,39 @@
+// Importar los módulos necesarios
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+// Crear la aplicación de Express
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Manejar la conexión de sockets
+io.on('connection', (socket) => {
+  console.log('Un cliente se ha conectado');
+
+  // Manejar el evento de compartir ubicación
+  socket.on('shareLocation', (location) => {
+    console.log('Ubicación recibida:', location);
+    // Aquí puedes realizar cualquier lógica adicional con la ubicación recibida
+
+    // Emitir la ubicación a todos los demás clientes conectados
+    socket.broadcast.emit('locationShared', location);
+  });
+
+  // Manejar la desconexión de sockets
+  socket.on('disconnect', () => {
+    console.log('Un cliente se ha desconectado');
+  });
+});
+
+// Iniciar el servidor
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
